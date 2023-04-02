9db289f5e54311f8e9155b41b70472277418a60b
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-04-02 16:53:07 -0400

    added percent change fxn to StatsService

--- src/services/StatsService.ts
+++ src/services/StatsService.ts
@@ -1,6 +1,7 @@
 import moment from "moment";
 import { ObjectId } from "mongodb";
 import { LogService } from "./ApiService/lib/LogService";
+import _ from "lodash";
 
 // we represent these in total minutes
 export enum timeInterval {
@@ -30,5 +31,28 @@ export const StatsService = {
       })
     );
     return logFrequencyArr;
+  },
+  getPercentChangeInFrequencyOfMostRecentLogs: async (
+    folderId: string,
+    interval: timeInterval,
+    stepsBack: number
+  ) => {
+    const logs = await StatsService.getLogFrequenciesByInterval(
+      folderId,
+      interval,
+      stepsBack
+    );
+    if (logs.length <= 1) {
+      return 0;
+    }
+
+    const mostRecentChunk = logs[0];
+    const sumOfOtherChunks = _.sum(logs.slice(1));
+    const averageOfOtherChunks = _.round(
+      sumOfOtherChunks / (logs.length - 1),
+      2
+    );
+    const diff = _.round(mostRecentChunk - averageOfOtherChunks, 2);
+    return _.round(100.0 * (diff / averageOfOtherChunks), 2);
   },
 };
--- src/tests/utils/StatsService.test.ts
+++ src/tests/utils/StatsService.test.ts
@@ -44,3 +44,108 @@ describe("GetLogFrequenciesByInterval", () => {
     expect(frequencyArr[2]).toBe(0);
   });
 });
+
+describe("GetPercentChangeInFrequencyOfMostRecentLogs", () => {
+  it("correctly gets the percentage change in log frequencies by an interval of an hour", async () => {
+    const folder = await FolderFactory.create();
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(5, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(42, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(56, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(65, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(88, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(89, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(95, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(1298, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      createdAt: moment().subtract(30, "minutes").toDate(),
+    });
+    const change =
+      await StatsService.getPercentChangeInFrequencyOfMostRecentLogs(
+        folder._id.toString(),
+        timeInterval.Hour,
+        3
+      );
+    expect(change).toBe(50);
+  });
+  it("correctly gets the percentage change in log frequencies by an interval of an hour", async () => {
+    const folder = await FolderFactory.create();
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(5, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(42, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(56, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(65, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(88, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(89, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(95, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(95, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(95, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(95, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      folderId: folder._id,
+      createdAt: moment().subtract(1298, "minutes").toDate(),
+    });
+    await LogFactory.create({
+      createdAt: moment().subtract(30, "minutes").toDate(),
+    });
+    const change =
+      await StatsService.getPercentChangeInFrequencyOfMostRecentLogs(
+        folder._id.toString(),
+        timeInterval.Hour,
+        3
+      );
+    expect(change).toBe(-14.29);
+  });
+});
