2e426288774b8a60a6b33338c6ab479c87d26470
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-04-17 22:20:06 -0400

    Update runRules.ts

--- src/jobs/runRules.ts
+++ src/jobs/runRules.ts
@@ -4,8 +4,8 @@ import { Logger } from "src/utils/logger";
 
 export const runRulesJob = async () => {
   Logger.sendLog("lookkkk ", "/debugging");
-  const organizations = await Organization.find({}, { _id: 1 }).lean().exec();
-  Logger.sendLog("hereeeee " + organizations.length, "/debugging");
+  const organizations = await Organization.find();
+  Logger.sendLog("hereeeee ", "/debugging");
   await Promise.all(
     organizations.map((organization) =>
       RuleService.runAllRulesForOrganization(organization._id.toString())
