6b336b7b2851a19a3d2955f29bf2086f0ba78669
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-05-12 12:07:42 -0400

    Update SendgridService.ts

--- src/services/integrations/components/SendgridService.ts
+++ src/services/integrations/components/SendgridService.ts
@@ -1,22 +1,20 @@
+import axios from "axios";
 import {
   IntegrationDocument,
-  OrganizationDocument,
-  integrationTypeEnum,
   keyTypeEnum,
+  OrganizationDocument,
   simplifiedLogTagEnum,
 } from "logtree-types";
-import { Integration } from "src/models/Integration";
-import { ApiError } from "src/utils/errors";
-import { SecureIntegrationService } from "../SecureIntegrationService";
-import axios from "axios";
+import moment from "moment";
 import {
   MAX_NUM_CHARS_ALLOWED_IN_LOG,
   SimplifiedLog,
 } from "src/services/ApiService/lib/LogService";
-import _ from "lodash";
-import { IntegrationServiceType } from "../types";
+import { ApiError } from "src/utils/errors";
 import { getFloorLogRetentionDateForOrganization } from "src/utils/helpers";
-import moment from "moment";
+
+import { SecureIntegrationService } from "../SecureIntegrationService";
+import { IntegrationServiceType } from "../types";
 
 const BASE_URL = "https://api.sendgrid.com/v3";
 
@@ -41,6 +39,81 @@ export const SendgridService: IntegrationServiceType = {
     const floorDate = getFloorLogRetentionDateForOrganization(organization);
     const headers = SendgridService.getHeaders(integration);
 
+    // let messageEvents: SimplifiedLog[] = [];
+    // await Promise.all([
+    //   async () => {
+    //     const blocksRes = await axios.get(
+    //       BASE_URL + "/suppression/blocks/" + query,
+    //       {
+    //         headers,
+    //       }
+    //     );
+
+    //     const { reason, created } = blocksRes.data;
+    //     const createdAt = new Date(created * 1000);
+    //     if (created && moment(createdAt).isSameOrAfter(floorDate)) {
+    //       messageEvents.push({
+    //         _id: `sendgrid_${uuid()}`,
+    //         content: `Email sent to ${query} was blocked because ${reason}`,
+    //         createdAt,
+    //         tag: simplifiedLogTagEnum.Error,
+    //         sourceTitle: "Sendgrid",
+    //       });
+    //     }
+    //   },
+    //   async () => {
+    //     const bounceRes = await axios.get(
+    //       BASE_URL + "/suppression/bounces/" + query,
+    //       {
+    //         headers,
+    //       }
+    //     );
+
+    //     const { reason, created } = bounceRes.data;
+    //     const createdAt = new Date(created * 1000);
+    //     if (created && moment(createdAt).isSameOrAfter(floorDate)) {
+    //       messageEvents.push({
+    //         _id: `sendgrid_${uuid()}`,
+    //         content: `Email sent to ${query} was bounced because ${reason}`,
+    //         createdAt,
+    //         tag: simplifiedLogTagEnum.Error,
+    //         sourceTitle: "Sendgrid",
+    //       });
+    //     }
+    //   },
+    //   async () => {
+    //     const suppressionsRes = await axios.get(
+    //       BASE_URL + "/suppression/unsubscribes",
+    //       {
+    //         params: {
+    //           start_time: floorDate.getTime() / 1000,
+    //           end_time: new Date().getTime() / 1000,
+    //         },
+    //         headers,
+    //       }
+    //     );
+
+    //     const suppressions = suppressionsRes.data;
+    //     let suppressionForUser = suppressions.find(
+    //       (suppression) => suppression.email === query
+    //     );
+    //     if (!suppressionForUser) {
+    //       return;
+    //     }
+
+    //     const createdAt = new Date(suppressionForUser.created * 1000);
+    //     if (moment(createdAt).isSameOrAfter(floorDate)) {
+    //       messageEvents.push({
+    //         _id: `sendgrid_${uuid()}`,
+    //         content: `${query} unsubscribed from all future emails.`,
+    //         createdAt,
+    //         tag: simplifiedLogTagEnum.Marketing,
+    //         sourceTitle: "Sendgrid",
+    //       });
+    //     }
+    //   },
+    // ]);
+
     const allMessagesRes = await axios.get(BASE_URL + "/messages", {
       params: {
         query: `query=to_email="${query}"`,
@@ -64,7 +137,11 @@ export const SendgridService: IntegrationServiceType = {
           if (moment(event.processed).isSameOrAfter(floorDate)) {
             messageEvents.push({
               _id: `sendgrid_${message.msg_id}_${i}`,
-              content: `Email to ${query} was ${event.event_name} because ${event.reason}.\n\nEmail subject: ${event.subject}`,
+              content:
+                `Email to ${query} was ${event.event_name} because ${event.reason}.\n\nEmail subject: ${event.subject}`.slice(
+                  0,
+                  MAX_NUM_CHARS_ALLOWED_IN_LOG
+                ),
               createdAt: new Date(event.processed),
               tag: simplifiedLogTagEnum.Marketing,
               sourceTitle: "Sendgrid",
