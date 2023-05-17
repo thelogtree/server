e9e643fdb83d2c5c03f9744fb482fc8cb8da2480
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-05-17 13:33:45 -0400

    Update QuestionAnswerService.ts

--- src/services/ApiService/lib/QuestionAnswerService.ts
+++ src/services/ApiService/lib/QuestionAnswerService.ts
@@ -72,15 +72,17 @@ export const QuestionAnswerService = {
       );
     const logsFromIntegration = await getLogsFxn!(integration);
 
-    const promptPrefix = `I am giving you all of the events from one business's ${integration.type} account:\n\n`;
+    const promptPrefix = `You are an analyst assistant bot called "Logtree AI". Your job is to help an analyst answer their question about a bunch of events from a specific source which could be storing marketing data, technical data, sales data, communications data, or anything else. I am giving you all of the events from the source they have a question about which is called ${integration.type}:\n\n`;
     const promptDataInput = _arrayToMarkdown(logsFromIntegration).slice(
       0,
-      3000
+      7000
     );
-    const promptQuestion = `\n\nHere is their question: ${question}\n\nYou must use reasoning and possibly math to make a useful and insightful response to the question. It is important to remember that the person asking the question does not know the structure of the data I am giving you, so do not speak too technically. Be nice with your response, and only include the most important details.`;
+    const promptQuestion = `\n\nHere is their question: ${question}\n\nNote that the person asking this question actually does not know the structure of the events I am giving you, so do not refer to any specific columns in the dataset. Please provide a response that I can send to this analyst.`;
 
     const response = await QuestionAnswerService.getCompletionResponse(
-      `${promptPrefix}${promptDataInput}${promptQuestion}`
+      `${promptPrefix}${promptDataInput}${promptQuestion}`,
+      1,
+      300
     );
 
     Logger.sendLog(
@@ -93,12 +95,12 @@ export const QuestionAnswerService = {
   },
   getCompletionResponse: async (
     prompt: string,
-    maxTokens?: number,
-    temperature?: number
+    temperature?: number,
+    maxTokens?: number
   ) => {
     const completion = await OpenAI.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
+      messages: [{ role: "assistant", content: prompt }],
       n: 1,
       temperature,
       max_tokens: maxTokens,
