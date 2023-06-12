c9eab362304c946078ee17f642ef45fc663e0576
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-06-12 09:21:53 -0700

    Update promoCodes.ts

--- src/utils/promoCodes.ts
+++ src/utils/promoCodes.ts
@@ -4,4 +4,5 @@ type PromoCodeMap = { [key in string]: number };
 // must all be lowercased. we convert codes to lowercase before they pass through here.
 export const AvailablePromoCodes = {
   zfellows: 80000,
+  v1: 20000,
 };
